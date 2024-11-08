e5fc255debb1d9649f9b98d79d523f2a5241b2dc
JonnelleC <carrjonnelle@gmail.com>
2024-11-08 02:39:55 -0500

    edit

--- drone-deploy/src/DroneDataTable.jsx
+++ drone-deploy/src/DroneDataTable.jsx
@@ -1,46 +1,16 @@
-import PropTypes from "prop-types";
+
+import PropTypes from 'prop-types';
+import DroneData from './src/DroneData';
 
 const DroneDataTable = ({ data }) => (
-  <div className="drone-table-container">
-    <table className="drone-table">
-      <thead>
-        <tr>
-          <th>Image ID</th>
-          <th>Timestamp</th>
-          <th>Latitude</th>
-          <th>Longitude</th>
-          <th>Altitude (m)</th>
-          <th>Battery (%)</th>
-          <th>Tags</th>
-          <th>File Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(
-          ({
-            image_id,
-            timestamp,
-            latitude,
-            longitude,
-            altitude_m,
-            battery_level_pct,
-            image_tags,
-            file_name,
-          }) => (
-            <tr key={image_id}>
-              <td>{image_id}</td>
-              <td>{timestamp}</td>
-              <td>{latitude}</td>
-              <td>{longitude}</td>
-              <td>{altitude_m}</td>
-              <td>{battery_level_pct}</td>
-              <td>{image_tags.join(", ")}</td>
-              <td>{file_name}</td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </table>
+  <div>
+    {data.map(({ image_id, latitude, longitude }) => (
+      <div key={image_id}>
+        <p>Image ID: {image_id}</p>
+        <p>Latitude: {latitude}</p>
+        <p>Longitude: {longitude}</p>
+      </div>
+    ))}
   </div>
 );
 
@@ -53,9 +23,13 @@ DroneDataTable.propTypes = {
       longitude: PropTypes.string.isRequired,
       altitude_m: PropTypes.number.isRequired,
       battery_level_pct: PropTypes.number.isRequired,
-      image_tags: PropTypes.arrayOf(PropTypes.string).isRequired, 
+      image_tags: PropTypes.arrayOf(PropTypes.string).isRequired,
       file_name: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
-export default DroneDataTable;
+
+const App = () => <DroneDataTable data={DroneData} />;
+
+export default App;
+
